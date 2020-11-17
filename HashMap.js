084db2ecb89b8225beb5e7258f9b2459c16ca59e
William Y Wong <wwong1987@gmail.com>
2020-11-17 11:02:01 -0800

    added get, delete and resize function to hashmap constructor

--- HashMap.js
+++ HashMap.js
@@ -1,5 +1,5 @@
 // implementing a hash map
-"use strict";
+'use strict';
 
 class HashMap {
   constructor(initialCapacity = 8) {
@@ -9,20 +9,19 @@ class HashMap {
     this._deleted = 0;
   }
 
-  static _hashString(string) {
-    // takes a string and hashes it, outputting a number (djb2 algorithm)
-    let hash = 5381;
-    for (let i = 0; i < string.length; i++) {
-      hash = (hash << 5) + hash + string.charCodeAt(i);
-      hash = hash & hash;
+  get(key) {
+    const index = this._findSlot(key);
+    if (this._hashTable[index] === undefined) {
+      throw new Error('Key error');
     }
-    return hash >>> 0;
+    return this._hashTable[index].value;
   }
 
   // Adding items to a hash map. The set() function initially checks whether the load ratio is greater than the given maximum. If so it resizes the hash map using the private _resize() function. This has an O(1) best and average case, and an O(n) worst case (if collisions occur)
   set(key, value) {
     const loadRatio = (this.length + this._deleted + 1) / this._capacity;
-    if (loadRatio > HashMap.MAX_LOAD_RATIO) { // MAX_LOAD_RATIO = keeps track of how full the hashmap is. When it is a certain % full, we move to a bigger hash table using the SIZE_RATIO so we reduce the number of collisions. Helps to improve the performance of value retrieval
+    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+      // MAX_LOAD_RATIO = keeps track of how full the hashmap is. When it is a certain % full, we move to a bigger hash table using the SIZE_RATIO so we reduce the number of collisions. Helps to improve the performance of value retrieval
       this._resize(this._capacity * HashMap.SIZE_RATIO);
     }
     //Find the slot where this key should be in
@@ -38,6 +37,17 @@ class HashMap {
     };
   }
 
+  delete(key) {
+    const index = this._findSlot(key);
+    const slot = this._hashTable[index];
+    if (slot === undefined) {
+      throw new Error('Key error');
+    }
+    slot.DELETE = true;
+    this.length--;
+    this._deleted++;
+  }
+
   // private helper function used to find the correct slot for a given key
   // best and avg-case performance for _findSlot() is O(1); assuming the hash function is good and the load ratio is suitable, then the chances of ocllisions should be low. Worst case is O(n), as you would have to linearly search through each slot.
   _findSlot(key) {
@@ -47,10 +57,35 @@ class HashMap {
     // loops through the array, stopping when it finds the slot with a matching key or an empty slot
     for (let i = start; i < start + this._capacity; i++) {
       const index = i % this._capacity;
-      const slot = this._hashTable[index]; 
+      const slot = this._hashTable[index];
       if (slot === undefined || (slot.key === key && !slot.DELETED)) {
         return index;
       }
     }
+  }
+
+  _resize(size) {
+    const oldSlots = this._hashTable;
+    this._capacity = size;
+    // Reset the length - it will get rebuilt as you add the items abck
+    this.length = 0;
+    this._deleted = 0;
+    this._hashTable = [];
+
+    for (const slot of oldSlots) {
+      if (slot !== undefined && !slot.DELETED) {
+        this.set(slot.key, slot.value);
+      }
+    }
+  }
+
+  static _hashString(string) {
+    // takes a string and hashes it, outputting a number (djb2 algorithm)
+    let hash = 5381;
+    for (let i = 0; i < string.length; i++) {
+      hash = (hash << 5) + hash + string.charCodeAt(i);
+      hash = hash & hash;
+    }
+    return hash >>> 0;
   }
 }
